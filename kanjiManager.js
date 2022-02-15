8fff6f0a851174b9982594f0ee17a4f529bfd40a
Michael Nix <michael.g.nix@gmail.com>
2022-02-15 17:30:13 -0600

    Started working on kanji loading and drawing

---
+++ kanjiManager.js
@@ -0,0 +1,52 @@
+const serializer = new XMLSerializer();
+
+function getUnicodeHex(char)
+{
+    let code = char.charCodeAt(0).toString(16)
+
+    while (code.length < 5)
+    {
+        code = "0" + code;
+    }
+
+    return code;
+}
+
+async function loadKanji(kanji, slot)
+{
+    let kanjiCode = getUnicodeHex(kanji);
+
+    console.log("Loading Kanji " + kanji);
+    console.log("Kanji Code: " + kanjiCode);
+
+    let kanjiResponse = await fetch("https://raw.githubusercontent.com/KanjiVG/kanjivg/master/kanji/" + kanjiCode + ".svg");
+
+    if (kanjiResponse.ok)
+    {
+        console.log("Loaded kanji " + kanji + " properly");
+    }
+
+    else
+    {
+        console.error("Failed to load kanji " + kanji + " properly: " + kanjiResponse.status + ": " + kanjiResponse.statusText);
+    }
+
+    let svgString = await kanjiResponse.text();
+
+    console.log("Response text: " + svgString);
+
+    const parser = new DOMParser();
+    let kanjiDoc = parser.parseFromString(svgString, "image/svg+xml");
+
+    kanjiDoc.getElementById("kvg:StrokeNumbers_" + kanjiCode).remove(); // Remove stroke numbers
+
+    let svgNode = kanjiDoc.getElementsByTagName("svg")[0];
+
+    svgNode.setAttribute("width", "100%");
+    svgNode.setAttribute("height", "100%");
+    //svgNode.height = "";
+
+    document.getElementById("kanji-" + slot).getElementsByClassName("strokes")[0].innerHTML = serializer.serializeToString(svgNode);
+}
+
+loadKanji("日", 0);
